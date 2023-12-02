b38c7f8e0a30385d20b15da519cb233ea556b3fc
yuehunagmatias <yue1786812727@gmail.com>
2023-12-02 12:02:28 -0300

    agrego logo en menu lateral y animacion para menu

--- Vista/HomePage.Designer.cs
+++ Vista/HomePage.Designer.cs
@@ -30,16 +30,21 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HomePage));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelMenu = new System.Windows.Forms.Panel();
+            this.btnBaseDeDatos = new FontAwesome.Sharp.IconButton();
+            this.btnUsuario = new FontAwesome.Sharp.IconButton();
+            this.iconButton4 = new FontAwesome.Sharp.IconButton();
+            this.btnProveedor = new FontAwesome.Sharp.IconButton();
+            this.btnProducto = new FontAwesome.Sharp.IconButton();
             this.btnCompra = new FontAwesome.Sharp.IconButton();
             this.btnVenta = new FontAwesome.Sharp.IconButton();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnProducto = new FontAwesome.Sharp.IconButton();
-            this.btnProveedor = new FontAwesome.Sharp.IconButton();
-            this.iconButton4 = new FontAwesome.Sharp.IconButton();
-            this.btnUsuario = new FontAwesome.Sharp.IconButton();
-            this.btnBaseDeDatos = new FontAwesome.Sharp.IconButton();
-            this.panel2.SuspendLayout();
+            this.panelLogo = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelMenu.SuspendLayout();
+            this.panelLogo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -47,110 +52,151 @@
             resources.ApplyResources(this.panel1, "panel1");
             this.panel1.Name = "panel1";
             // 
-            // panel2
+            // panelMenu
             // 
-            this.panel2.BackColor = System.Drawing.Color.Coral;
-            this.panel2.Controls.Add(this.btnBaseDeDatos);
-            this.panel2.Controls.Add(this.btnUsuario);
-            this.panel2.Controls.Add(this.iconButton4);
-            this.panel2.Controls.Add(this.btnProveedor);
-            this.panel2.Controls.Add(this.btnProducto);
-            this.panel2.Controls.Add(this.btnCompra);
-            this.panel2.Controls.Add(this.btnVenta);
-            this.panel2.Controls.Add(this.panel3);
-            resources.ApplyResources(this.panel2, "panel2");
-            this.panel2.Name = "panel2";
-            // 
-            // btnCompra
-            // 
-            resources.ApplyResources(this.btnCompra, "btnCompra");
-            this.btnCompra.FlatAppearance.BorderSize = 0;
-            this.btnCompra.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
-            this.btnCompra.IconColor = System.Drawing.Color.White;
-            this.btnCompra.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnCompra.IconSize = 32;
-            this.btnCompra.Name = "btnCompra";
-            this.btnCompra.UseVisualStyleBackColor = true;
-            // 
-            // btnVenta
-            // 
-            resources.ApplyResources(this.btnVenta, "btnVenta");
-            this.btnVenta.FlatAppearance.BorderSize = 0;
-            this.btnVenta.IconChar = FontAwesome.Sharp.IconChar.SackDollar;
-            this.btnVenta.IconColor = System.Drawing.Color.White;
-            this.btnVenta.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnVenta.IconSize = 32;
-            this.btnVenta.Name = "btnVenta";
-            this.btnVenta.UseVisualStyleBackColor = true;
-            // 
-            // panel3
-            // 
-            resources.ApplyResources(this.panel3, "panel3");
-            this.panel3.Name = "panel3";
-            // 
-            // btnProducto
-            // 
-            resources.ApplyResources(this.btnProducto, "btnProducto");
-            this.btnProducto.FlatAppearance.BorderSize = 0;
-            this.btnProducto.IconChar = FontAwesome.Sharp.IconChar.Shopify;
-            this.btnProducto.IconColor = System.Drawing.Color.White;
-            this.btnProducto.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnProducto.IconSize = 32;
-            this.btnProducto.Name = "btnProducto";
-            this.btnProducto.UseVisualStyleBackColor = true;
-            // 
-            // btnProveedor
-            // 
-            resources.ApplyResources(this.btnProveedor, "btnProveedor");
-            this.btnProveedor.FlatAppearance.BorderSize = 0;
-            this.btnProveedor.IconChar = FontAwesome.Sharp.IconChar.UserAlt;
-            this.btnProveedor.IconColor = System.Drawing.Color.White;
-            this.btnProveedor.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnProveedor.IconSize = 32;
-            this.btnProveedor.Name = "btnProveedor";
-            this.btnProveedor.UseVisualStyleBackColor = true;
-            // 
-            // iconButton4
-            // 
-            resources.ApplyResources(this.iconButton4, "iconButton4");
-            this.iconButton4.FlatAppearance.BorderSize = 0;
-            this.iconButton4.IconChar = FontAwesome.Sharp.IconChar.SquarePollVertical;
-            this.iconButton4.IconColor = System.Drawing.Color.White;
-            this.iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.iconButton4.IconSize = 32;
-            this.iconButton4.Name = "iconButton4";
-            this.iconButton4.UseVisualStyleBackColor = true;
-            // 
-            // btnUsuario
-            // 
-            resources.ApplyResources(this.btnUsuario, "btnUsuario");
-            this.btnUsuario.FlatAppearance.BorderSize = 0;
-            this.btnUsuario.IconChar = FontAwesome.Sharp.IconChar.User;
-            this.btnUsuario.IconColor = System.Drawing.Color.White;
-            this.btnUsuario.IconFont = FontAwesome.Sharp.IconFont.Auto;
-            this.btnUsuario.IconSize = 32;
-            this.btnUsuario.Name = "btnUsuario";
-            this.btnUsuario.UseVisualStyleBackColor = true;
+            this.panelMenu.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelMenu.Controls.Add(this.btnBaseDeDatos);
+            this.panelMenu.Controls.Add(this.btnUsuario);
+            this.panelMenu.Controls.Add(this.iconButton4);
+            this.panelMenu.Controls.Add(this.btnProveedor);
+            this.panelMenu.Controls.Add(this.btnProducto);
+            this.panelMenu.Controls.Add(this.btnCompra);
+            this.panelMenu.Controls.Add(this.btnVenta);
+            this.panelMenu.Controls.Add(this.panelLogo);
+            resources.ApplyResources(this.panelMenu, "panelMenu");
+            this.panelMenu.Name = "panelMenu";
             // 
             // btnBaseDeDatos
             // 
+            this.btnBaseDeDatos.BackColor = System.Drawing.Color.MidnightBlue;
             resources.ApplyResources(this.btnBaseDeDatos, "btnBaseDeDatos");
             this.btnBaseDeDatos.FlatAppearance.BorderSize = 0;
+            this.btnBaseDeDatos.ForeColor = System.Drawing.Color.White;
             this.btnBaseDeDatos.IconChar = FontAwesome.Sharp.IconChar.BarChart;
             this.btnBaseDeDatos.IconColor = System.Drawing.Color.White;
             this.btnBaseDeDatos.IconFont = FontAwesome.Sharp.IconFont.Auto;
             this.btnBaseDeDatos.IconSize = 32;
             this.btnBaseDeDatos.Name = "btnBaseDeDatos";
-            this.btnBaseDeDatos.UseVisualStyleBackColor = true;
+            this.btnBaseDeDatos.UseVisualStyleBackColor = false;
+            this.btnBaseDeDatos.Click += new System.EventHandler(this.btnBaseDeDatos_Click);
+            // 
+            // btnUsuario
+            // 
+            this.btnUsuario.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.btnUsuario, "btnUsuario");
+            this.btnUsuario.FlatAppearance.BorderSize = 0;
+            this.btnUsuario.ForeColor = System.Drawing.Color.White;
+            this.btnUsuario.IconChar = FontAwesome.Sharp.IconChar.User;
+            this.btnUsuario.IconColor = System.Drawing.Color.White;
+            this.btnUsuario.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnUsuario.IconSize = 32;
+            this.btnUsuario.Name = "btnUsuario";
+            this.btnUsuario.UseVisualStyleBackColor = false;
+            this.btnUsuario.Click += new System.EventHandler(this.btnUsuario_Click);
+            // 
+            // iconButton4
+            // 
+            this.iconButton4.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.iconButton4, "iconButton4");
+            this.iconButton4.FlatAppearance.BorderSize = 0;
+            this.iconButton4.ForeColor = System.Drawing.Color.White;
+            this.iconButton4.IconChar = FontAwesome.Sharp.IconChar.SquarePollVertical;
+            this.iconButton4.IconColor = System.Drawing.Color.White;
+            this.iconButton4.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.iconButton4.IconSize = 32;
+            this.iconButton4.Name = "iconButton4";
+            this.iconButton4.UseVisualStyleBackColor = false;
+            this.iconButton4.Click += new System.EventHandler(this.iconButton4_Click);
+            // 
+            // btnProveedor
+            // 
+            this.btnProveedor.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.btnProveedor, "btnProveedor");
+            this.btnProveedor.FlatAppearance.BorderSize = 0;
+            this.btnProveedor.ForeColor = System.Drawing.Color.White;
+            this.btnProveedor.IconChar = FontAwesome.Sharp.IconChar.UserAlt;
+            this.btnProveedor.IconColor = System.Drawing.Color.White;
+            this.btnProveedor.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnProveedor.IconSize = 32;
+            this.btnProveedor.Name = "btnProveedor";
+            this.btnProveedor.UseVisualStyleBackColor = false;
+            this.btnProveedor.Click += new System.EventHandler(this.btnProveedor_Click);
+            // 
+            // btnProducto
+            // 
+            this.btnProducto.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.btnProducto, "btnProducto");
+            this.btnProducto.FlatAppearance.BorderSize = 0;
+            this.btnProducto.ForeColor = System.Drawing.Color.White;
+            this.btnProducto.IconChar = FontAwesome.Sharp.IconChar.Shopify;
+            this.btnProducto.IconColor = System.Drawing.Color.White;
+            this.btnProducto.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnProducto.IconSize = 32;
+            this.btnProducto.Name = "btnProducto";
+            this.btnProducto.UseVisualStyleBackColor = false;
+            this.btnProducto.Click += new System.EventHandler(this.btnProducto_Click);
+            // 
+            // btnCompra
+            // 
+            this.btnCompra.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.btnCompra, "btnCompra");
+            this.btnCompra.FlatAppearance.BorderSize = 0;
+            this.btnCompra.ForeColor = System.Drawing.Color.White;
+            this.btnCompra.IconChar = FontAwesome.Sharp.IconChar.ShoppingCart;
+            this.btnCompra.IconColor = System.Drawing.Color.White;
+            this.btnCompra.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnCompra.IconSize = 32;
+            this.btnCompra.Name = "btnCompra";
+            this.btnCompra.UseVisualStyleBackColor = false;
+            this.btnCompra.Click += new System.EventHandler(this.btnCompra_Click);
+            // 
+            // btnVenta
+            // 
+            this.btnVenta.BackColor = System.Drawing.Color.MidnightBlue;
+            resources.ApplyResources(this.btnVenta, "btnVenta");
+            this.btnVenta.FlatAppearance.BorderSize = 0;
+            this.btnVenta.ForeColor = System.Drawing.Color.White;
+            this.btnVenta.IconChar = FontAwesome.Sharp.IconChar.SackDollar;
+            this.btnVenta.IconColor = System.Drawing.Color.White;
+            this.btnVenta.IconFont = FontAwesome.Sharp.IconFont.Auto;
+            this.btnVenta.IconSize = 32;
+            this.btnVenta.Name = "btnVenta";
+            this.btnVenta.UseVisualStyleBackColor = false;
+            this.btnVenta.Click += new System.EventHandler(this.btnVenta_Click);
+            // 
+            // panelLogo
+            // 
+            this.panelLogo.BackColor = System.Drawing.Color.MidnightBlue;
+            this.panelLogo.Controls.Add(this.pictureBox2);
+            this.panelLogo.Controls.Add(this.pictureBox1);
+            resources.ApplyResources(this.panelLogo, "panelLogo");
+            this.panelLogo.Name = "panelLogo";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::Vista.Properties.Resources.Star_CG;
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
             // 
             // HomePage
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panelMenu);
             this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.Gainsboro;
             this.Name = "HomePage";
-            this.panel2.ResumeLayout(false);
+            this.panelMenu.ResumeLayout(false);
+            this.panelLogo.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -158,14 +204,16 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panelMenu;
         private FontAwesome.Sharp.IconButton btnCompra;
         private FontAwesome.Sharp.IconButton btnVenta;
-        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panelLogo;
         private FontAwesome.Sharp.IconButton btnBaseDeDatos;
         private FontAwesome.Sharp.IconButton btnUsuario;
         private FontAwesome.Sharp.IconButton iconButton4;
         private FontAwesome.Sharp.IconButton btnProveedor;
         private FontAwesome.Sharp.IconButton btnProducto;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
--- Vista/HomePage.cs
+++ Vista/HomePage.cs
@@ -7,14 +7,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using FontAwesome.Sharp;
 
 namespace Vista
 {
     public partial class HomePage : Form
     {
+        private IconButton currentBtn;
+        private Panel leftBorderBtn;
         public HomePage()
         {
             InitializeComponent();
+            leftBorderBtn= new Panel();
+            leftBorderBtn.Size = new Size(7, 60);
+            panelMenu.Controls.Add(leftBorderBtn);
+        }
+        private struct RGBColors
+        {
+            public static Color color1 = Color.FromArgb(172, 126, 241);
+            public static Color color2 = Color.FromArgb(249, 118, 176);
+            public static Color color3 = Color.FromArgb(253, 138, 114);
+            public static Color color4 = Color.FromArgb(95, 77, 221);
+            public static Color color5 = Color.FromArgb(249, 88, 155);
+            public static Color color6 = Color.FromArgb(24,161,251);
+            public static Color color7 = Color.FromArgb(52, 88, 114);
+        }
+
+
+        private void ActivateButton(object senderBtn, Color color)
+        {
+            if (senderBtn != null)
+            {
+                DisableButton();
+                // Button
+                currentBtn = (IconButton)senderBtn;
+                currentBtn.BackColor = Color.FromArgb(37,36,81);
+                currentBtn.ForeColor = color;
+                currentBtn.TextAlign = ContentAlignment.MiddleCenter;
+                currentBtn.IconColor= color;
+                currentBtn.TextImageRelation = TextImageRelation.TextBeforeImage;
+                currentBtn.ImageAlign=ContentAlignment.MiddleRight;
+                // Left border
+                leftBorderBtn.BackColor = color;
+                leftBorderBtn.Location= new Point(0,currentBtn.Location.Y);
+                leftBorderBtn.Visible = true;
+                leftBorderBtn.BringToFront();
+
+
+            }
+        }
+
+        private void DisableButton()
+        {
+            if(currentBtn != null)
+            {
+                currentBtn.BackColor = Color.FromArgb(31, 30, 68);
+                currentBtn.ForeColor = Color.Black;
+                currentBtn.TextAlign = ContentAlignment.MiddleLeft;
+                currentBtn.IconColor = Color.White;
+                currentBtn.TextImageRelation = TextImageRelation.ImageBeforeText;
+                currentBtn.ImageAlign = ContentAlignment.MiddleLeft;
+
+            }
         }
 
         private void panelMenu_Paint(object sender, PaintEventArgs e)
@@ -25,6 +79,41 @@ namespace Vista
         private void iconButton3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void btnVenta_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color1);
+        }
+
+        private void btnCompra_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color2);
+        }
+
+        private void btnProducto_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color3);
+        }
+
+        private void btnProveedor_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color4);
+        }
+
+        private void iconButton4_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color5);
+        }
+
+        private void btnUsuario_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color6);
+        }
+
+        private void btnBaseDeDatos_Click(object sender, EventArgs e)
+        {
+            ActivateButton(sender, RGBColors.color7);
         }
     }
 }
