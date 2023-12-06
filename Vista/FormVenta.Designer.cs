eb888c0fbbfcb5257f6c70f135a31d30ff4d8491
yuehunagmatias <yue1786812727@gmail.com>
2023-12-06 12:41:58 -0300

    ya anda la busqueda y calculo de precio venta

--- Vista/FormVenta.Designer.cs
+++ Vista/FormVenta.Designer.cs
@@ -28,20 +28,253 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panelDetalle = new System.Windows.Forms.Panel();
+            this.btnCerrarVenta = new System.Windows.Forms.Button();
+            this.btnBuscarProducto = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtPrecioDetalleVenta = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregarDetallePedido = new System.Windows.Forms.Button();
+            this.txtNombDetalle = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtCodigoDetalle = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblPorcentaje = new System.Windows.Forms.Label();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.panelDetalle.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panelDetalle
+            // 
+            this.panelDetalle.BackColor = System.Drawing.Color.White;
+            this.panelDetalle.Controls.Add(this.dataGridView1);
+            this.panelDetalle.Controls.Add(this.btnCerrarVenta);
+            this.panelDetalle.Controls.Add(this.btnBuscarProducto);
+            this.panelDetalle.Controls.Add(this.groupBox2);
+            this.panelDetalle.Controls.Add(this.label10);
+            this.panelDetalle.Controls.Add(this.txtCodigoDetalle);
+            this.panelDetalle.Location = new System.Drawing.Point(12, 12);
+            this.panelDetalle.Name = "panelDetalle";
+            this.panelDetalle.Size = new System.Drawing.Size(897, 558);
+            this.panelDetalle.TabIndex = 19;
+            // 
+            // btnCerrarVenta
+            // 
+            this.btnCerrarVenta.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnCerrarVenta.Font = new System.Drawing.Font("Century", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrarVenta.ForeColor = System.Drawing.Color.Black;
+            this.btnCerrarVenta.Location = new System.Drawing.Point(762, 401);
+            this.btnCerrarVenta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnCerrarVenta.Name = "btnCerrarVenta";
+            this.btnCerrarVenta.Size = new System.Drawing.Size(131, 154);
+            this.btnCerrarVenta.TabIndex = 18;
+            this.btnCerrarVenta.Text = "Finalizar la compra";
+            this.btnCerrarVenta.UseVisualStyleBackColor = false;
+            // 
+            // btnBuscarProducto
+            // 
+            this.btnBuscarProducto.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnBuscarProducto.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscarProducto.ForeColor = System.Drawing.Color.Black;
+            this.btnBuscarProducto.Location = new System.Drawing.Point(601, 6);
+            this.btnBuscarProducto.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnBuscarProducto.Name = "btnBuscarProducto";
+            this.btnBuscarProducto.Size = new System.Drawing.Size(170, 63);
+            this.btnBuscarProducto.TabIndex = 17;
+            this.btnBuscarProducto.Text = "Buscar Producto";
+            this.btnBuscarProducto.UseVisualStyleBackColor = false;
+            this.btnBuscarProducto.Click += new System.EventHandler(this.btnBuscarProducto_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox2.Controls.Add(this.txtPorcentaje);
+            this.groupBox2.Controls.Add(this.lblPorcentaje);
+            this.groupBox2.Controls.Add(this.txtPrecioDetalleVenta);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtCantidad);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.btnAgregarDetallePedido);
+            this.groupBox2.Controls.Add(this.txtNombDetalle);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(67, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(689, 258);
+            this.groupBox2.TabIndex = 16;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Datos de Productos";
+            // 
+            // txtPrecioDetalleVenta
+            // 
+            this.txtPrecioDetalleVenta.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtPrecioDetalleVenta.Enabled = false;
+            this.txtPrecioDetalleVenta.Location = new System.Drawing.Point(265, 129);
+            this.txtPrecioDetalleVenta.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPrecioDetalleVenta.Name = "txtPrecioDetalleVenta";
+            this.txtPrecioDetalleVenta.Size = new System.Drawing.Size(284, 28);
+            this.txtPrecioDetalleVenta.TabIndex = 17;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 129);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 19);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Precio :";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtCantidad.Location = new System.Drawing.Point(265, 182);
+            this.txtCantidad.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(224, 28);
+            this.txtCantidad.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(12, 188);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(165, 19);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Ingresar Cantidad :";
+            // 
+            // btnAgregarDetallePedido
+            // 
+            this.btnAgregarDetallePedido.BackColor = System.Drawing.Color.Cornsilk;
+            this.btnAgregarDetallePedido.ForeColor = System.Drawing.Color.Black;
+            this.btnAgregarDetallePedido.Location = new System.Drawing.Point(531, 188);
+            this.btnAgregarDetallePedido.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.btnAgregarDetallePedido.Name = "btnAgregarDetallePedido";
+            this.btnAgregarDetallePedido.Size = new System.Drawing.Size(151, 54);
+            this.btnAgregarDetallePedido.TabIndex = 12;
+            this.btnAgregarDetallePedido.Text = "Agregar Detalle Pedido";
+            this.btnAgregarDetallePedido.UseVisualStyleBackColor = false;
+            this.btnAgregarDetallePedido.Click += new System.EventHandler(this.btnAgregarDetallePedido_Click);
+            // 
+            // txtNombDetalle
+            // 
+            this.txtNombDetalle.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtNombDetalle.Location = new System.Drawing.Point(265, 42);
+            this.txtNombDetalle.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtNombDetalle.Name = "txtNombDetalle";
+            this.txtNombDetalle.Size = new System.Drawing.Size(284, 28);
+            this.txtNombDetalle.TabIndex = 2;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(12, 42);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(86, 19);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Nombre :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.SystemColors.Control;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(41, 14);
+            this.label10.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(176, 19);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Codigo De Producto";
+            // 
+            // txtCodigoDetalle
+            // 
+            this.txtCodigoDetalle.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtCodigoDetalle.Location = new System.Drawing.Point(288, 14);
+            this.txtCodigoDetalle.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtCodigoDetalle.Name = "txtCodigoDetalle";
+            this.txtCodigoDetalle.Size = new System.Drawing.Size(284, 25);
+            this.txtCodigoDetalle.TabIndex = 14;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(71, 351);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(684, 184);
+            this.dataGridView1.TabIndex = 19;
+            // 
+            // lblPorcentaje
+            // 
+            this.lblPorcentaje.AutoSize = true;
+            this.lblPorcentaje.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lblPorcentaje.ForeColor = System.Drawing.Color.Black;
+            this.lblPorcentaje.Location = new System.Drawing.Point(12, 86);
+            this.lblPorcentaje.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPorcentaje.Name = "lblPorcentaje";
+            this.lblPorcentaje.Size = new System.Drawing.Size(194, 19);
+            this.lblPorcentaje.TabIndex = 20;
+            this.lblPorcentaje.Text = "Porcentaje Ganancia :";
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtPorcentaje.Location = new System.Drawing.Point(265, 86);
+            this.txtPorcentaje.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.Size = new System.Drawing.Size(284, 28);
+            this.txtPorcentaje.TabIndex = 21;
             // 
             // FormVenta
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(921, 545);
+            this.ClientSize = new System.Drawing.Size(996, 599);
+            this.Controls.Add(this.panelDetalle);
             this.Name = "FormVenta";
             this.Text = "FormVenta";
             this.Load += new System.EventHandler(this.FormVenta_Load);
+            this.panelDetalle.ResumeLayout(false);
+            this.panelDetalle.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panelDetalle;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnCerrarVenta;
+        private System.Windows.Forms.Button btnBuscarProducto;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.TextBox txtPrecioDetalleVenta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregarDetallePedido;
+        private System.Windows.Forms.TextBox txtNombDetalle;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtCodigoDetalle;
+        private System.Windows.Forms.Label lblPorcentaje;
+        private System.Windows.Forms.TextBox txtPorcentaje;
     }
 }
--- Vista/FormVenta.cs
+++ Vista/FormVenta.cs
@@ -7,21 +7,128 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Controladora;
+using Entidades;
 
 namespace Vista
 {
     public partial class FormVenta : Form
     {
         private int RoleID;
+        private int CodigoEncontrado;
+        private decimal porcentaje;
+        private decimal precioCompra;
+        private decimal precioVenta;
         public FormVenta(int RoleID)
         {
             InitializeComponent();
             this.RoleID = RoleID;
+            vaciarTextbox();
+            txtPorcentaje.Text = "0.4";
         }
 
         private void FormVenta_Load(object sender, EventArgs e)
         {
+            permiso();
+        }
 
+        private void btnBuscarProducto_Click(object sender, EventArgs e)
+        {
+            vaciarTextbox();
+            if (string.IsNullOrEmpty(txtCodigoDetalle.Text))
+            {
+                MessageBox.Show("Ingresar código de producto deseado por favor.");
+            }
+            else
+            {
+                int codigoProducto;
+                if (!int.TryParse(txtCodigoDetalle.Text, out codigoProducto))
+                {
+                    MessageBox.Show("Por favor, ingresar un código de producto válido.");
+                    return;
+                }
+
+                ControlProducto controlProducto = new ControlProducto();
+                Producto productoBuscado = new Producto { Codigo = codigoProducto };
+
+                Producto productoEncontrado = controlProducto.BuscarProductoPorCodigo(productoBuscado);
+
+                if (productoEncontrado != null)
+                {
+                    if (productoEncontrado.ID_Estado != 0 || RoleID == 1)
+                    {// Mostrar la información del producto en los controles correspondientes
+                        porcentaje=decimal.Parse(txtPorcentaje.Text);
+                        precioCompra = Convert.ToDecimal( productoEncontrado.Price);
+                        MessageBox.Show(precioCompra.ToString());
+                        CodigoEncontrado = int.Parse(txtCodigoDetalle.Text);
+                        txtNombDetalle.Text = productoEncontrado.Name;
+                        precioVenta = precioCompra * (1 + porcentaje);
+                        txtPrecioDetalleVenta.Text = (Math.Round(precioVenta, 2, MidpointRounding.AwayFromZero).ToString());
+                        // Establecer la variable de estado a true si se encontró el producto
+                    }
+                    else
+                    {
+                        MessageBox.Show("Producto Buscado esta dado de baja, porfavor comunicarse con el admin ");
+                        vaciarTextbox();
+
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Producto no encontrado.");
+                }
+            }
+        }
+
+        private long ObtenerIDCompraActual()
+        {
+            // Utilizar la fecha y hora actual para generar un identificador único
+            // Puedes ajustar el formato según tus necesidades
+            string formatoID = "yyyyMMddHHmmss";
+            string idCompra = DateTime.Now.ToString(formatoID);
+
+            // Convertir el ID a un valor numérico (opcional, dependiendo de tu implementación en la base de datos)
+            if (long.TryParse(idCompra, out long resultado))
+            {
+                return resultado;
+            }
+            else
+            {
+                // Manejar el caso en el que la conversión no sea exitosa
+                // Podrías lanzar una excepción, retornar un valor predeterminado, etc.
+                return 0; // Valor predeterminado, ajusta según tu lógica
+            }
+        }
+
+        private void vaciarTextbox()
+        {
+            txtCantidad.Text=String.Empty;
+            txtNombDetalle.Text = string.Empty;
+            txtPrecioDetalleVenta.Text = string.Empty;
+        }
+
+
+
+
+        private void permiso()
+        {
+            if (RoleID == 1)
+            {
+                lblPorcentaje.Visible = true;
+                txtPorcentaje.Visible =true;
+            }
+            else
+            {
+                lblPorcentaje.Visible = false;
+                txtPorcentaje.Visible = false;
+                txtPorcentaje.Text = "0,40";
+            }
+        }
+
+        private void btnAgregarDetallePedido_Click(object sender, EventArgs e)
+        {
+            
         }
     }
 }
