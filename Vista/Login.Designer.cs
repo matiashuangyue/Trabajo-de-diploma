1761355cbc31281ef63ca62bfb0403c6105081bf
YUE HUANG <yue1786812727@gmail.com>
2024-05-02 18:27:51 -0300

    termino este commit y arranco con login

--- Vista/Login.Designer.cs
+++ Vista/Login.Designer.cs
@@ -42,10 +42,10 @@
             // 
             this.btnLogin.BackColor = System.Drawing.Color.Cornsilk;
             this.btnLogin.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnLogin.Location = new System.Drawing.Point(210, 165);
-            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.btnLogin.Location = new System.Drawing.Point(186, 170);
+            this.btnLogin.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
             this.btnLogin.Name = "btnLogin";
-            this.btnLogin.Size = new System.Drawing.Size(184, 59);
+            this.btnLogin.Size = new System.Drawing.Size(207, 71);
             this.btnLogin.TabIndex = 1;
             this.btnLogin.Text = "LOGIN";
             this.btnLogin.UseVisualStyleBackColor = false;
@@ -53,28 +53,28 @@
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(123, 104);
-            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtPassword.Location = new System.Drawing.Point(138, 125);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
             this.txtPassword.Name = "txtPassword";
             this.txtPassword.PasswordChar = '*';
-            this.txtPassword.Size = new System.Drawing.Size(288, 28);
+            this.txtPassword.Size = new System.Drawing.Size(324, 33);
             this.txtPassword.TabIndex = 8;
             // 
             // txtDNI
             // 
-            this.txtDNI.Location = new System.Drawing.Point(123, 22);
-            this.txtDNI.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.txtDNI.Location = new System.Drawing.Point(138, 26);
+            this.txtDNI.Margin = new System.Windows.Forms.Padding(3, 6, 3, 6);
             this.txtDNI.Name = "txtDNI";
-            this.txtDNI.Size = new System.Drawing.Size(288, 28);
+            this.txtDNI.Size = new System.Drawing.Size(324, 33);
             this.txtDNI.TabIndex = 7;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.BackColor = System.Drawing.Color.Transparent;
-            this.label2.Location = new System.Drawing.Point(27, 107);
+            this.label2.Location = new System.Drawing.Point(30, 128);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(109, 24);
+            this.label2.Size = new System.Drawing.Size(107, 25);
             this.label2.TabIndex = 6;
             this.label2.Text = "Password";
             // 
@@ -83,9 +83,9 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.ForeColor = System.Drawing.SystemColors.MenuText;
-            this.label1.Location = new System.Drawing.Point(27, 27);
+            this.label1.Location = new System.Drawing.Point(30, 32);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 24);
+            this.label1.Size = new System.Drawing.Size(49, 25);
             this.label1.TabIndex = 5;
             this.label1.Text = "DNI";
             // 
@@ -95,9 +95,9 @@
             this.label3.BackColor = System.Drawing.Color.Cornsilk;
             this.label3.Font = new System.Drawing.Font("Perpetua Titling MT", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(65, 9);
+            this.label3.Location = new System.Drawing.Point(177, 9);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(675, 33);
+            this.label3.Size = new System.Drawing.Size(797, 39);
             this.label3.TabIndex = 9;
             this.label3.Text = "SISTEMA DE CONTROL Y GESTION DE STOCK";
             this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -111,20 +111,22 @@
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.btnLogin);
             this.panel1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.panel1.Location = new System.Drawing.Point(177, 86);
+            this.panel1.Location = new System.Drawing.Point(330, 54);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(426, 242);
+            this.panel1.Size = new System.Drawing.Size(508, 283);
             this.panel1.TabIndex = 10;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // Login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(786, 371);
+            this.ClientSize = new System.Drawing.Size(1104, 445);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.label3);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Login";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "LOGIN";
