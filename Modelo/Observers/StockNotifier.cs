471d5e1273ab8596315a44070ab481443f490362
YUE HUANG <yue1786812727@gmail.com>
2024-08-03 14:32:05 -0300

    agregue stocknotifier pero todavia no implemente

---
+++ Modelo/Observers/StockNotifier.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entidades;
+using Modelo.Interfaces;
+
+namespace Modelo.Observers
+{
+    public class StockNotifier 
+    {
+        public void Update(Producto producto)
+        {
+            if (producto.Stock < 10) // Umbral de stock bajo, por ejemplo 10
+            {
+                // Enviar alerta o tomar alguna acción
+                Console.WriteLine($"Alerta: El stock del producto {producto.Name} es bajo. Stock actual: {producto.Stock}");
+            }
+        }
+
+
+
+    }
+}
