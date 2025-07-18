5023a223f8dbe4dea12211bf1038832ea35d5bb9
YUE HUANG <yue1786812727@gmail.com>
2025-07-18 16:12:46 -0300

    agregue clase DetallePedido En entidades

---
+++ Entidades/DetallePedido.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DetallePedido
+    {
+        // ATRIBUTOS PRIVADOS
+        //
+        private long detalleID;
+        private long id_pedido;
+        private int id_producto;
+        private int cantidad;
+        private decimal precioVenta;
+        private decimal cantidadPrecio;
+
+        // PROPIEDADES PÚBLICAS
+        //
+
+
+        public long DetalleID { get; set; }
+        public long ID_Pedido { get; set; }
+        public int ID_Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal PrecioVenta { get; set; }
+        public decimal CantidadPrecio { get; set; }
+
+    }
+}
--- Entidades/Pedido.cs
+++ Entidades/Pedido.cs
@@ -31,28 +31,6 @@ namespace Entidades
         public string MetodoPago { get; set; } // Nuevo campo para el método de pago
 
     }
-    public class DetallePedido
-    {
-        // ATRIBUTOS PRIVADOS
-        //
-        private long detalleID;
-        private long id_pedido;
-        private int id_producto;
-        private int cantidad;
-        private decimal precioVenta;
-        private decimal cantidadPrecio;
 
-        // PROPIEDADES PÚBLICAS
-        //
-
-
-        public long DetalleID { get; set; }
-        public long ID_Pedido { get; set; }
-        public int ID_Producto { get; set; }
-        public int Cantidad { get; set; }
-        public decimal PrecioVenta { get; set; }
-        public decimal CantidadPrecio { get; set; }
-
-    }
 }
     
