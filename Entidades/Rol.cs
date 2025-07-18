fdfe3153ac98a7085b62a3cc3de8854b4d137316
YUE HUANG <yue1786812727@gmail.com>
2025-07-18 11:56:01 -0300

    agregue entidades

---
+++ Entidades/Rol.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Rol
+    {
+       
+        public string Nombre { get; set; } 
+    }
+}
