dc6445aa807db8e0dcc3daf2cfa943878786106c
YUE HUANG <yue1786812727@gmail.com>
2025-07-18 16:09:43 -0300

    agregue clase DetalleCompra En entidades

--- Entidades/Compra.cs
+++ Entidades/Compra.cs
@@ -56,31 +56,5 @@ namespace Entidades
             }
         }
 
-    public class DetalleCompra
-    {
-        // ATRIBUTOS PRIVADOS
-        //
-        private long id_compra;
-        private int id_producto;
-        private int cantidad;
-        private decimal precioUnitario;
-        private string nombreProducto;
-        // PROPIEDADES PÚBLICAS
-        //
-
-        public long ID_Compra { get; set; }
-        public int ID_Producto { get; set; }
-
-        public int Cantidad { get; set; }
-        public decimal PrecioUnitario { get; set; }
-
-        public string NombreProducto { get; set; }
-        public decimal PrecioTotal
-        {
-            get
-            {
-                return Cantidad * PrecioUnitario;
-            }
-        }
-    }
+ 
 }
---
+++ Entidades/DetalleCompra.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class DetalleCompra
+    {
+        // ATRIBUTOS PRIVADOS
+        //
+        private long id_compra;
+        private int id_producto;
+        private int cantidad;
+        private decimal precioUnitario;
+        private string nombreProducto;
+        // PROPIEDADES PÚBLICAS
+        //
+
+        public long ID_Compra { get; set; }
+        public int ID_Producto { get; set; }
+
+        public int Cantidad { get; set; }
+        public decimal PrecioUnitario { get; set; }
+
+        public string NombreProducto { get; set; }
+        public decimal PrecioTotal
+        {
+            get
+            {
+                return Cantidad * PrecioUnitario;
+            }
+        }
+    }
+}
